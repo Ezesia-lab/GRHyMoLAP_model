74ad6bd19c51e3bc30674f41ce2f66aeeb663967
Ezesia-lab <ezeckiel.sianou@students.jkuat.ac.ke>
2026-09-16 17:35:18 +0200

    Update A_CAMELS_FR_basin.py

--- examples/A_CAMELS_FR_basin.py
+++ examples/A_CAMELS_FR_basin.py
@@ -25,25 +25,40 @@ meta, dataframe = rr.fetch()
 from grhymolap import GRHyMoLAP
 
 #Station and data
+station = 'H050301001'
 
-station ='M334091010'
+data = dataframe[station].sel(time=slice("2000-01-01", "2021-12-31"))
 
-Q     = dataframe[station].sel(dynamic_features="q_mm_obs").to_numpy()
-P     = dataframe[station].sel(dynamic_features="pcp_mm").to_numpy()
-PET   = dataframe[station].sel(dynamic_features="pet_mm_pm").to_numpy()
+Q = data.sel(dynamic_features="q_mm_obs").to_numpy()
+P = data.sel(dynamic_features="pcp_mm").to_numpy()
+PET = data.sel(dynamic_features="pet_mm_pm").to_numpy()
 
-
-dates = pd.date_range(start="2000-01-01", end="2021-12-31", freq="D")
+dates = data.time.to_numpy()
 
 
 #Fitting
+model = GRHyMoLAP(
+    n_warmup=0,
+    objective="nse",
+    optimizer="nelder-mead",
+    optimizer_kwargs={"options": {"maxiter": 2500, "disp": False}},
+)
 
-model = GRHyMoLAP(n_warmup=0, objective="nse", optimizer="nelder-mead")
-model.fit(P, PET, Q, dates=dates, train_ratio=0.6)
+model.fit(
+    P,
+    PET,
+    Q,
+    dates=dates,
+    calibration_period=("2000-01-01", "2012-12-31"),
+    validation_period=("2013-01-01", "2021-12-31"),
+)
 
 print("Calibrated params (MU, LAMBDA, X1, gamma):", model.params_)
-print("Train scores:", model.train_scores_)
-print("Val scores:  ", model.val_scores_)
+
+print("Calibration scores:", model.calibration_scores_)
+
+print("Validation scores:  ", model.validation_scores_)
+
 
 #Plot
 dates = pd.date_range(
@@ -56,6 +71,9 @@ Qsim = model.simulate(P, PET)
 
 mask = (dates >= "2020-01-01") & (dates <= "2021-12-31")
 
+nse_val = model.validation_scores_["nse"]
+kge_val = model.validation_scores_["kge"]
+
 plt.figure(figsize=(14, 5))
 
 plt.plot(dates[mask], Q[mask], label="Observed", linewidth=1)
@@ -63,6 +81,16 @@ plt.plot(dates[mask], Qsim[mask], label="Simulated", linewidth=1)
 
 plt.xlabel("Date")
 plt.ylabel("Streamflow (mm/day)")
+
+plt.text(
+    0.22,
+    0.95,
+    f"Station: H050301001\nNSE_val = {nse_val:.3f}\nKGE_val = {kge_val:.3f}",
+    transform=plt.gca().transAxes,
+    verticalalignment="top",
+    bbox=dict(facecolor="white", alpha=0.8)
+)
+
 plt.legend()
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
